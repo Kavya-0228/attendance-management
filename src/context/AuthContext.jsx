283d874e4f402f85c2bbf4a94d55e6205ca95c5c
Kavya-0228 <kavyaanjalai26@gmail.com>
2026-03-03 09:31:43 +0530

    Fix issue

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -94,7 +94,7 @@ export const AuthProvider = ({ children }) => {
         loading,
         login,
         register,
-        logout,
+        logout,git
         isAuthenticated: !!token,
     };
 
